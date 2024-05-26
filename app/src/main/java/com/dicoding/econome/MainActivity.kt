e8747d2377221f18715ce950773f1b231353eee9
nurwandhika <nurwan.dhika.rachman@gmail.com>
2024-05-26 15:43:53 +0700

    penambahan fitur delete dan juga snackbar untuk transaction

--- app/src/main/java/com/dicoding/econome/MainActivity.kt
+++ app/src/main/java/com/dicoding/econome/MainActivity.kt
@@ -2,15 +2,21 @@ package com.dicoding.econome
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import com.dicoding.econome.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var deleteTransaction: Transaction
+    private lateinit var oldTransactions: List<Transaction>
     private lateinit var binding: ActivityMainBinding
     private lateinit var transactions: List<Transaction>
     private lateinit var transactionAdapter: TransactionAdapter
@@ -38,6 +44,27 @@ class MainActivity : AppCompatActivity() {
             layoutManager = this@MainActivity.linearLayoutManager
         }
 
+        val itemTouchHelper = object :
+            ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: androidx.recyclerview.widget.RecyclerView,
+                viewHolder: androidx.recyclerview.widget.RecyclerView.ViewHolder,
+                target: androidx.recyclerview.widget.RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(
+                viewHolder: androidx.recyclerview.widget.RecyclerView.ViewHolder,
+                direction: Int
+            ) {
+                deleteTransaction(transactions[viewHolder.adapterPosition])
+            }
+
+        }
+
+        val swipeHelper = ItemTouchHelper(itemTouchHelper)
+        swipeHelper.attachToRecyclerView(binding.rvTransactions)
 
         binding.addTransactionFAB.setOnClickListener {
             val intent = Intent(this, AddTransactionActivity::class.java)
@@ -64,6 +91,45 @@ class MainActivity : AppCompatActivity() {
         binding.tvBalanceAmount.text = "Rp %.0f".format(balanceAmount)
         binding.tvIncomeAmount.text = "Rp %.0f".format(incomeAmount)
         binding.tvExpenseAmount.text = "Rp %.0f".format(expenseAmount)
+    }
+
+    private fun undoDelete(){
+        GlobalScope.launch {
+            db.transactionDao().insertAll(deleteTransaction)
+            transactions = oldTransactions
+            runOnUiThread {
+                transactionAdapter.setData(transactions)
+                updateDashboard()
+            }
+        }
+    }
+
+    private fun showSnackbar() {
+        val view = findViewById<View>(R.id.main)
+        val snackbar = Snackbar.make(view, "Transaction deleted", Snackbar.LENGTH_LONG)
+        snackbar.setAction("Undo") {
+            undoDelete()
+        }
+            .setActionTextColor(ContextCompat.getColor(this, R.color.red))
+            .setTextColor(ContextCompat.getColor(this, R.color.white))
+            .setAnchorView(binding.addTransactionFAB)
+            .show()
+    }
+
+    private fun deleteTransaction(transaction: Transaction) {
+        deleteTransaction = transaction
+        oldTransactions = transactions
+
+        GlobalScope.launch {
+            db.transactionDao().delete(transaction)
+
+            transactions = transactions.filter { it.id != transaction.id }
+            runOnUiThread {
+                updateDashboard()
+                transactionAdapter.setData(transactions)
+                showSnackbar()
+            }
+        }
     }
 
     override fun onResume() {
